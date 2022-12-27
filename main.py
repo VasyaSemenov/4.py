2a3a07e8d128f2b3bcf21a5e17626faad45e1e43
dolbognom1 <112643093+dolbognom1@users.noreply.github.com>
2022-12-27 20:39:41 +0500

    программа написана

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ with open("data-20200320T1016-structure-20200320T1016.csv","r") as file:
 	reader = csv.reader(file, delimiter=",")
 	dataset = [x for x in reader]
 keys = list(keys.split(","))
-women_passengers = list(map(lambda x: x[4] == 'Визовый', dataset))
+women_passengers = list(map(lambda x: x[4] == 'Безвизовый', dataset))
 print(women_passengers.count(True))
